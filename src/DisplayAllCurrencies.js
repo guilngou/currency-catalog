f03ef18f11e33666cefa3a3a311047cf69bda7e1
Alexandre <alexandreguilngar@gmail.com>
2018-10-01 18:43:31 +0200

    implemented pagination

--- src/DisplayAllCurrencies.js
+++ src/DisplayAllCurrencies.js
@@ -7,25 +7,139 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { withRouter } from "react-router";
 import Card from "@material-ui/core/Card";
 import { getAllCurrenciesMethod } from "./getAllCurrencies";
+import Select from "@material-ui/core/Select";
+import TablePagination from "@material-ui/core/TablePagination";
+import { withStyles } from "@material-ui/core/styles";
+import FirstPageIcon from "@material-ui/icons/FirstPage";
+import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
+import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
+import LastPageIcon from "@material-ui/icons/LastPage";
+import IconButton from "@material-ui/core/IconButton";
+import PropTypes from "prop-types";
+
+const actionsStyles = theme => ({
+  root: {
+    flexShrink: 0,
+    color: theme.palette.text.secondary,
+    marginLeft: theme.spacing.unit * 2.5
+  }
+});
+
+class TablePaginationActions extends React.Component {
+  handleFirstPageButtonClick = event => {
+    this.props.onChangePage(event, 0);
+  };
+
+  handleBackButtonClick = event => {
+    this.props.onChangePage(event, this.props.page - 1);
+  };
+
+  handleNextButtonClick = event => {
+    this.props.onChangePage(event, this.props.page + 1);
+  };
+
+  handleLastPageButtonClick = event => {
+    this.props.onChangePage(
+      event,
+      Math.max(0, Math.ceil(this.props.count / this.props.rowsPerPage) - 1)
+    );
+  };
+
+  render() {
+    const { classes, count, page, rowsPerPage, theme } = this.props;
+    return (
+      <div className={classes.root}>
+        <IconButton
+          onClick={this.handleFirstPageButtonClick}
+          disabled={page === 0}
+          aria-label="First Page"
+        >
+          {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
+        </IconButton>
+        <IconButton
+          onClick={this.handleBackButtonClick}
+          disabled={page === 0}
+          aria-label="Previous Page"
+        >
+          {theme.direction === "rtl" ? (
+            <KeyboardArrowRight />
+          ) : (
+            <KeyboardArrowLeft />
+          )}
+        </IconButton>
+        <IconButton
+          onClick={this.handleNextButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="Next Page"
+        >
+          {theme.direction === "rtl" ? (
+            <KeyboardArrowLeft />
+          ) : (
+            <KeyboardArrowRight />
+          )}
+        </IconButton>
+        <IconButton
+          onClick={this.handleLastPageButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="Last Page"
+        >
+          {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
+        </IconButton>
+      </div>
+    );
+  }
+}
+
+TablePaginationActions.propTypes = {
+  classes: PropTypes.object.isRequired,
+  count: PropTypes.number.isRequired,
+  onChangePage: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+  theme: PropTypes.object.isRequired
+};
+
+const TablePaginationActionsWrapped = withStyles(actionsStyles, {
+  withTheme: true
+})(TablePaginationActions);
+
+const styles = theme => ({
+  root: {
+    width: "100%",
+    marginTop: theme.spacing.unit * 3
+  },
+  table: {
+    minWidth: 500
+  },
+  tableWrapper: {
+    overflowX: "auto"
+  }
+});
 
 class DisplayAllCurrencies extends Component {
   constructor() {
     super();
     this.state = {
-      currencies: []
+      currencies: [],
+      rowsPerPage: 10,
+      page: 0
     };
   }
 
+  handleChangePage = (event, page) => {
+    this.setState({ page });
+  };
+
+  handleChangeRowsPerPage = event => {
+    this.setState({ rowsPerPage: event.target.value });
+  };
+
   async componentWillMount() {
     try {
+      const { rowsPerPage, page } = this.state;
       const currenciesFetch = await getAllCurrenciesMethod();
-      console.log(
-        "currenciesFetch.data : " + JSON.stringify(currenciesFetch.data)
-      );
-
       const currenciesList = Object.values(currenciesFetch.data);
       const currencies = currenciesList.map((currency, i) => {
-        console.log("currency : " + JSON.stringify(currency));
         return (
           <Grid item xs={6}>
             <Card>
@@ -43,14 +157,18 @@ class DisplayAllCurrencies extends Component {
         );
       });
       this.setState({ currencies: currencies });
-      console.log("currencies : " + JSON.stringify(currencies));
-      console.log("this.state: " + JSON.stringify(this.state));
     } catch (e) {
       console.error(e);
     }
   }
 
   render() {
+    const { classes } = this.props;
+    const { currencies, rowsPerPage, page } = this.state;
+    const emptyRows =
+      rowsPerPage -
+      Math.min(rowsPerPage, currencies.length - page * rowsPerPage);
+
     return (
       <MuiThemeProvider>
         <div className="App">
@@ -58,11 +176,25 @@ class DisplayAllCurrencies extends Component {
             <h1 className="App-title">Available currencies</h1>
           </header>
           <Grid container spacing={24}>
-            {this.state.currencies}
+            {this.state.currencies.slice(
+              page * rowsPerPage,
+              page * rowsPerPage + rowsPerPage
+            )}
           </Grid>
-        </div>
+        </div>{" "}
+        <TablePagination
+          colSpan={3}
+          count={this.state.currencies.length}
+          rowsPerPage={rowsPerPage}
+          rowsPerPageOptions={[10, 50, 100]}
+          page={page}
+          onChangePage={this.handleChangePage}
+          onChangeRowsPerPage={this.handleChangeRowsPerPage}
+          ActionsComponent={TablePaginationActionsWrapped}
+        />
       </MuiThemeProvider>
     );
   }
 }
+
 export const DisplayAllCurrenciesComponent = withRouter(DisplayAllCurrencies);
